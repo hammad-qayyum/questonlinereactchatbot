8b9d546a5fd9cb785b163e970a80852be3e45ced
Hammad Qayyum <hammadqayyum1047@gmail.com>
2024-08-11 00:02:02 +0500

    api change

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // const CHATBOT_BASE_URL="https://3ad9-39-46-141-202.ngrok-free.app"
 // const CHATBOT_BASE_URL="http://98.80.78.119:3201"
-const CHATBOT_BASE_URL="http://api-chatbot.questonline.co.uk/backend-chatbot"
+const CHATBOT_BASE_URL="https://api-chatbot.questonline.co.uk/backend-chatbot"
 
 
 
